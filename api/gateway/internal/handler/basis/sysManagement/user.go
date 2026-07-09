cf117cbb470eda571006305cdb5b6cbeb06853ad
pddzl <td27fish@163.com>
2026-07-09 18:18:08 +0800

    fix: GetUserInfo reads userId from JWT claims instead of URL query param

---
+++ api/gateway/internal/handler/basis/sysManagement/user.go
@@ -0,0 +1,128 @@
+package sysManagement
+
+import (
+	"context"
+	"net/http"
+
+	"td27/api/gateway/internal/middleware"
+	"td27/api/gateway/internal/svc"
+	"td27/pkg/api"
+	"td27/rpc/basis/types/common_pb"
+	"td27/rpc/basis/types/sysManagement/user_pb"
+)
+
+type UserHandler struct {
+	svcCtx *svc.ServiceContext
+}
+
+func NewUserHandler(svcCtx *svc.ServiceContext) *UserHandler {
+	return &UserHandler{svcCtx: svcCtx}
+}
+
+func (h *UserHandler) GetUserInfo(w http.ResponseWriter, r *http.Request) {
+	userId, _ := r.Context().Value(middleware.UserIdKey).(float64)
+	resp, err := h.svcCtx.UserClient.GetUserInfo(context.Background(), &common_pb.IdReq{Id: uint64(userId)})
+	if err != nil {
+		api.FailWithMessage(w, err.Error())
+		return
+	}
+	api.OkWithData(w, resp)
+}
+
+func (h *UserHandler) ListUser(w http.ResponseWriter, r *http.Request) {
+	var req common_pb.PageReq
+	if err := api.DecodeAndValidate(r.Body, &req); err != nil {
+		api.FailWithRequest(w, http.StatusBadRequest, "invalid request body")
+		return
+	}
+	resp, err := h.svcCtx.UserClient.ListUser(context.Background(), &req)
+	if err != nil {
+		api.FailWithMessage(w, err.Error())
+		return
+	}
+	api.OkWithData(w, resp)
+}
+
+func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
+	var req user_pb.CreateUserReq
+	if err := api.DecodeAndValidate(r.Body, &req); err != nil {
+		api.FailWithRequest(w, http.StatusBadRequest, "invalid request body")
+		return
+	}
+	resp, err := h.svcCtx.UserClient.CreateUser(context.Background(), &req)
+	if err != nil {
+		api.FailWithMessage(w, err.Error())
+		return
+	}
+	api.OkWithData(w, resp)
+}
+
+func (h *UserHandler) UpdateUser(w http.ResponseWriter, r *http.Request) {
+	var req user_pb.UpdateUserReq
+	if err := api.DecodeAndValidate(r.Body, &req); err != nil {
+		api.FailWithRequest(w, http.StatusBadRequest, "invalid request body")
+		return
+	}
+	resp, err := h.svcCtx.UserClient.UpdateUser(context.Background(), &req)
+	if err != nil {
+		api.FailWithMessage(w, err.Error())
+		return
+	}
+	api.OkWithData(w, resp)
+}
+
+func (h *UserHandler) DeleteUser(w http.ResponseWriter, r *http.Request) {
+	var req common_pb.IdReq
+	if err := api.DecodeAndValidate(r.Body, &req); err != nil {
+		api.FailWithRequest(w, http.StatusBadRequest, "invalid request body")
+		return
+	}
+	resp, err := h.svcCtx.UserClient.DeleteUser(context.Background(), &req)
+	if err != nil {
+		api.FailWithMessage(w, err.Error())
+		return
+	}
+	api.OkWithData(w, resp)
+}
+
+func (h *UserHandler) ModifyPassword(w http.ResponseWriter, r *http.Request) {
+	var req user_pb.ModifyPasswdReq
+	if err := api.DecodeAndValidate(r.Body, &req); err != nil {
+		api.FailWithRequest(w, http.StatusBadRequest, "invalid request body")
+		return
+	}
+	resp, err := h.svcCtx.UserClient.ModifyPassword(context.Background(), &req)
+	if err != nil {
+		api.FailWithMessage(w, err.Error())
+		return
+	}
+	api.OkWithData(w, resp)
+}
+
+func (h *UserHandler) SwitchActive(w http.ResponseWriter, r *http.Request) {
+	var req user_pb.SwitchActiveReq
+	if err := api.DecodeAndValidate(r.Body, &req); err != nil {
+		api.FailWithRequest(w, http.StatusBadRequest, "invalid request body")
+		return
+	}
+	resp, err := h.svcCtx.UserClient.SwitchUserActive(context.Background(), &req)
+	if err != nil {
+		api.FailWithMessage(w, err.Error())
+		return
+	}
+	api.OkWithData(w, resp)
+}
+
+func (h *UserHandler) AssignRoles(w http.ResponseWriter, r *http.Request) {
+	var req user_pb.AssignRolesReq
+	if err := api.DecodeAndValidate(r.Body, &req); err != nil {
+		api.FailWithRequest(w, http.StatusBadRequest, "invalid request body")
+		return
+	}
+	resp, err := h.svcCtx.UserClient.AssignRoles(context.Background(), &req)
+	if err != nil {
+		api.FailWithMessage(w, err.Error())
+		return
+	}
+	api.OkWithData(w, resp)
+}
