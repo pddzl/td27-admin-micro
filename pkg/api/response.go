0c48f3e7921c66607a4cc8f572b7333779206f5b
pddzl <td27fish@163.com>
2026-07-10 14:00:07 +0800

    fix: use protojson for protobuf marshal - camelCase field names in JSON responses

--- pkg/api/response.go
+++ pkg/api/response.go
@@ -1,8 +1,11 @@
 package api
 
 import (
-	"github.com/zeromicro/go-zero/rest/httpx"
+	"encoding/json"
 	"net/http"
+
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
 )
 
 type Response struct {
@@ -17,12 +20,41 @@ const (
 	ERROR_RES = 7 // response/business error
 )
 
+func marshalJSON(v interface{}) ([]byte, error) {
+	if msg, ok := v.(proto.Message); ok {
+		return protojson.Marshal(msg)
+	}
+	return json.Marshal(v)
+}
+
 func result(w http.ResponseWriter, status int, code int, data interface{}, msg string) {
-	httpx.WriteJson(w, status, Response{
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(status)
+
+	resp := struct {
+		Code int             `json:"code"`
+		Data json.RawMessage `json:"data"`
+		Msg  string          `json:"msg"`
+	}{
 		Code: code,
-		Data: data,
 		Msg:  msg,
-	})
+	}
+
+	if data != nil {
+		b, err := marshalJSON(data)
+		if err != nil {
+			resp.Code = ERROR_RES
+			resp.Msg = "marshal error: " + err.Error()
+			resp.Data = json.RawMessage("null")
+		} else {
+			resp.Data = json.RawMessage(b)
+		}
+	} else {
+		resp.Data = json.RawMessage("{}")
+	}
+
+	full, _ := json.Marshal(resp)
+	w.Write(full)
 }
 
 func Ok(w http.ResponseWriter) {
