94970fbb759708131da4a20a1bfa19bbb2aa5309
pddzl <td27fish@163.com>
2026-07-10 14:37:19 +0800

    fix: add db:token_hash tag to ServiceToken, add db: tags to TokenPermission

--- rpc/basis/internal/model/sysTool/service_token.go
+++ rpc/basis/internal/model/sysTool/service_token.go
@@ -7,10 +7,10 @@ import (
 // ServiceToken Service-to-service authentication token entity
 type ServiceToken struct {
 	common.Td27Model
-	Name      string `json:"name" gorm:"size:100;unique;not null;comment:令牌名称/描述"`
-	TokenHash string `json:"-" gorm:"size:255;unique;not null;comment:令牌哈希值"`
-	Status    bool   `json:"status" gorm:"default:true;comment:是否启用"`
-	ExpiresAt *int64 `json:"expiresAt" gorm:"comment:过期时间戳(秒)"`
+	Name      string `json:"name" db:"name"`
+	TokenHash string `json:"-" db:"token_hash"`
+	Status    bool   `json:"status" db:"status"`
+	ExpiresAt *int64 `json:"expiresAt" db:"expires_at"`
 }
 
 func (ServiceToken) TableName() string {
@@ -19,8 +19,8 @@ func (ServiceToken) TableName() string {
 
 // TokenPermission Service token permission join table
 type TokenPermission struct {
-	TokenID      uint `json:"tokenId" gorm:"primaryKey"`
-	PermissionID uint `json:"permissionId" gorm:"primaryKey"`
+	TokenID      uint `json:"tokenId" db:"token_id"`
+	PermissionID uint `json:"permissionId" db:"permission_id"`
 }
 
 func (TokenPermission) TableName() string {
