86584745458abdc6f48668099d87c7549b0766d4
pddzl <td27fish@163.com>
2026-07-24 13:25:17 +0800

    fix: use updated_at (snake_case) instead of updatedAt in dictDetail template

--- web/src/pages/sysManagement/dict/dictDetail.vue
+++ web/src/pages/sysManagement/dict/dictDetail.vue
@@ -251,7 +251,7 @@ watch(
             </el-table-column> -->
             <el-table-column label="更新日期" width="180">
               <template #default="scope">
-                {{ formatDateTime(scope.row.updatedAt) }}
+                {{ formatDateTime(scope.row.updated_at) }}
               </template>
             </el-table-column>
             <el-table-column label="操作" width="260">
